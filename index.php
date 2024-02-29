2147d23760136c654e1701694b8d5708536edf84
hiagoassuncaoDev <hiago.amor03@gmail.com>
2024-02-29 20:25:18 -0300

    Adiciona a funcionalidade de login

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <style>
+        body {
+            background-color: #f2f2f2;
+        }
+
+        .login {
+            width: 100%;
+            height: 100vh;
+            align-items: center;
+            justify-content: center;
+            display: flex;
+        }
+    </style>
+</head>
+<body>
+    <div class="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="login.php" method="post">
+                                <label for="usuario">Usuário: </label>
+                                <input type="text" name="usuario"> <br> <br>
+                                <label for="senha">Senha: </label>
+                                <input type="password" name="senha"> <br> <br>
+                                <button type="submit" class="btn btn-primary">Fazer login</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ paginainicial.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+if(empty($_SESSION)){
+    echo
+    "<script>
+        location.href='index.php'
+    </script>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicial</title>
+</head>
+<body>
+    <h1>Você está logado</h1>
+    <a href="paginaadmin.php">Acesso Restrito</a>
+    <a href="logout.php">Sair</a>
+</body>
+</html>
