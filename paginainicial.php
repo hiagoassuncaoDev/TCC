031faa18fbd6bcde8941a8fe2be25d2ba5a336d1
hiagoassuncaoDev <hiago.amor03@gmail.com>
2024-03-01 01:50:06 -0300

    Novas atualizações no sistema e inclusão dos assets e style

--- paginainicial.php
+++ paginainicial.php
@@ -1,18 +1,18 @@
 <?php 
-session_start();
-if(empty($_SESSION)){
-    echo
-    "<script>
-        location.href='index.php'
-    </script>";
+    session_start();
+    if(empty($_SESSION)){
+        echo
+        "<script>
+            location.href='index.php'
+        </script>";
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>inicial</title>
+    <title>Página Inicial</title>
 </head>
 <body>
     <h1>Você está logado</h1>
